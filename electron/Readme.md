a9f2ef2e4ac899863fd371ff401e4f7bf3d2b7fb
Your Name <you@example.com>
2018-06-06 10:57:45 +0400

    Electron v2.0.1

--- electron/Readme.md
+++ electron/Readme.md
@@ -1,12 +1,16 @@
 ### Electron for Freebsd.
 
-Built with FreeBSD 11.0-RELEASE-p1. 
 ```
-Electron v1.8.2  
-Chromium v59.0.3071.115 
+Electron v2.0.1  
+Chromium v61.0.3163.100 
 ```
 
-**Building**
+Built with FreeBSD 11.1-RELEASE  
+Node v9.10.1  
+Npm 5.7.1  
+
+
+**Build**
 > sh build_electron.sh
 
 If the script completes without any errors, the Electron binary and zipped package will be placed in `$BUILDROOT/electron/dist/` folder.
--- electron/build_electron.sh
+++ electron/build_electron.sh
@@ -6,57 +6,49 @@ export BUILDROOT=`pwd`
 set -x
 ver=`uname -K`
 
-svnlite co -r446317 svn://svn.freebsd.org/ports/head/www/chromium chromium
+#Building libchromiumcontent chromium-61.0.3163.100
+echo "Building LIBCHROMIUMCONTENT ..."
+svnlite co -r456719 svn://svn.freebsd.org/ports/head/www/chromium chromium
 cd chromium
-rm files/patch-chrome_browser_ui_libgtkui_gtk_ui.cc
-make configure DISABLE_VULNERABILITIES=yes
+patch -p1  < ../chromium_make.diff
+make configure DISABLE_LICENSES=1 DISABLE_VULNERABILITIES=yes
 cd ..
 git clone https://github.com/electron/libchromiumcontent.git
 cd libchromiumcontent
-git checkout dbd83b6
-if [ "$ver" -lt 1100508 ]
-then
-	patch -p1 --ignore-whitespace < ../libchromiumcontent_110.diff
-else
-	patch -p1  --ignore-whitespace < ../libchromiumcontent_111.diff
-fi
+git checkout 0e760628832e77f72b4975ae0bcae8bb74afbf9c
+patch -p1  --ignore-whitespace < ../libchromiumcontent_111.diff
 script/bootstrap
-#59.0.3071.115
-mv ../chromium/work/chromium-59.0.3071.115 src
-mv src/third_party/ffmpeg/BUILD.gn.orig src/third_party/ffmpeg/BUILD.gn
+#61.0.3163.100
+mv ../chromium/work/chromium-61.0.3163.100 src
+patch -p1 --ignore-whitespace < ../libchromiumcontent_patches.diff
 patch -p1 --ignore-whitespace  -d src/ < ../chromiumv1.diff
-
-rm patches/third_party/skia/003-freetype-2.patch
-rm patches/third_party/swiftshader/001-gold_ifc.patch
-
+patch -p1 --ignore-whitespace  -d src/ < ../libchromiumcontent_bsd.diff
+patch -p1 --ignore-whitespace  -d src/ < ../libchromiumcontent_v8.diff
+rm patches/v8/025-cherry_pick_cc55747.patch*
+#sudo chown -R prash:wheel src/
 script/update -t x64 --skip_gclient
-script/build --no_shared_library -t x64
+script/build -c static_library -t x64
+script/build -c ffmpeg -t x64
+#script/build -c native_mksnapshot -t x64
 script/create-dist -c static_library -t x64
+#script/create-dist  -c native_mksnapshot -t x64
 
 #Building ELECTRON ...
 echo "Building ELECTRON ..."
 cd $BUILDROOT
 git clone https://github.com/electron/electron.git
 cd electron/
-git checkout v1.8.2
-if [ "$ver" -lt 1100508 ]
-then
-	patch  -p1 --ignore-whitespace < ../electron_110.diff
-else
-	patch  -p1 --ignore-whitespace < ../electron_111.diff
-fi
-
+git checkout v2.0.1
+patch  -p1 --ignore-whitespace < ../electron_111.diff
 set +e
 #this will fail when it tries to download libchromiumcontent library
 script/bootstrap.py -v --clang_dir=/usr
 set -e
-patch  -p1  --ignore-whitespace -d vendor/native_mate/  < ../electron_vendor_native_matev1.diff
 patch  -p1  --ignore-whitespace -d brightray/  < ../electron_brightrayv3.diff
 patch  -p1  --ignore-whitespace -d vendor/libchromiumcontent/  < ../electron_vendor_libchromiumcontentv1.diff
 mkdir vendor/download/libchromiumcontent
-unzip ../libchromiumcontent/libchromiumcontent.zip -d vendor/download/libchromiumcontent/
-unzip ../libchromiumcontent/libchromiumcontent-static.zip -d vendor/download/libchromiumcontent/
-
+tar -xvf ../libchromiumcontent/libchromiumcontent.tar.bz2 -C vendor/download/libchromiumcontent/
+tar -xvf ../libchromiumcontent/libchromiumcontent-static.tar.bz2 -C vendor/download/libchromiumcontent/
 script/bootstrap.py -v --clang_dir=/usr
 script/build.py -c R
 script/create-dist.py
